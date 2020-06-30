74258e04561b31bad7f1e2ce6f1851831d0ba79a
Cristian Penteado <cristianpenteado@protonmail.ch>
2020-06-29 22:56:13 -0300

    feat: add authentication middleware

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,7 @@
 import { Router } from 'express'
+
+import authMiddleware from './middlewares/authMiddleware'
+
 import UserController from './controllers/UsersController'
 import AuthController from './controllers/AuthController'
 
@@ -6,5 +9,7 @@ const router = Router()
 
 router.post('/users', UserController.store)
 router.post('/auth', AuthController.authenticate)
+router.get('/users', authMiddleware, UserController.index)
+
 
 export default router
